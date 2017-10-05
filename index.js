9ba1187e72aeb8aec3fcb74b6ec94625694263d8
Juan David Granada <jdgranada86@gmail.com>
2017-10-05 10:05:12 -0500

    include operations

--- index.js
+++ index.js
@@ -9,5 +9,41 @@ function sumar() {
   result.innerHTML = sum
 }
 
+function resta() {
+  var number1 = document.getElementById('number1').value
+  var number2 = document.getElementById('number2').value
+
+  var resta = parseInt(number1) - parseInt(number2)
+
+  var result = document.getElementById('result')
+
+  result.innerHTML = resta
+}
+
+function multiplica() {
+  var number1 = document.getElementById('number1').value
+  var number2 = document.getElementById('number2').value
+
+  var multiplica = parseInt(number1) * parseInt(number2)
+
+  var result = document.getElementById('result')
+
+  result.innerHTML = multiplica
+}
+
+
+function divide() {
+  var number1 = document.getElementById('number1').value
+  var number2 = document.getElementById('number2').value
+
+  var divide = parseInt(number1) / parseInt(number2)
+
+  var result = document.getElementById('result')
+
+  result.innerHTML = divide
+}
+
+
+
 
  
